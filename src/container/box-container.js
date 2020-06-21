c99f7a4c6ad2cd8deb2818473133b7fc445a35bd
Targas <targas@pr8.com.br>
2020-06-21 10:48:06 -0300

    feat: components

---
+++ src/container/box-container.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Box from '../component/box'
+
+class BoxCon extends React.Component {
+    render() {
+        return (
+            <div>
+                <Box></Box>
+            </div>
+        )
+    }
+}
+
+export default BoxCon
